0359678370ffb41b9dde57056fee25bce676261d
Tom McHenry <tommchenry@gmail.com>
2019-08-12 10:07:15 -0500

    Client displays mewls in reverse chrono order

--- app/controllers/mewls_controller.rb
+++ app/controllers/mewls_controller.rb
@@ -1,11 +1,11 @@
 class MewlsController < ApiController
   def index 
-    @mewls = Mewl.all
-    render json: @mewls.to_json
+    @mewls = Mewl.all.order('created_at DESC')
+    render json: @mewls, :include => {:user => {:only => [:username, :name]}}
   end
 
   def show
     @mewl = Mewl.find(params[:id])
-    render json: @mewl.to_json
+    render json: @mewl, :include => {:user => {:only => [:username, :name]}}
   end
 end
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Container, Item, Dimmer, Loader } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {}
+    this.getMewls = this.getMewls.bind(this)
+  }
+
+  componentDidMount () {
+    this.getMewls()
+  }
+
+  fetch (endpoint) {
+    return window.fetch(endpoint)
+      .then(response => response.json())
+      .catch(error => console.log(error))
+  }
+
+  getMewls () {
+    this.fetch('/api/mewls')
+      .then(mewls => {
+        if (mewls.length) {
+          this.setState({mewls: mewls})
+        } else {
+          this.setState({mewls: []})
+        }
+      })
+  }
+
+  render () {
+    let {mewls} = this.state
+    return mewls
+    ? <Container text>
+        <Item.Group divided unstackable>
+          {Object.keys(mewls).map((key) => (
+            <Item>
+              <Item.Content>
+              <Item.Header>@{mewls[key].user.username}</Item.Header>
+              <Item.Meta>{mewls[key].user.name}</Item.Meta>
+              <Item.Description>
+                <span>{mewls[key].text}</span>
+              </Item.Description>
+              </Item.Content>
+            </Item>
+          ))}
+        </Item.Group>
+      </Container>
+    : <Container text>
+        <Dimmer active inverted>
+          <Loader content='Loading' />
+        </Dimmer>
+      </Container>
+  }
 }
 
-export default App;
+export default App
