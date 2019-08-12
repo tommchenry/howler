949c18abbc863c71f460e0bbb7be7e164d9816b3
Tom McHenry <tommchenry@gmail.com>
2019-08-11 20:14:01 -0500

    Add annotate

--- Gemfile
+++ Gemfile
@@ -49,6 +49,8 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+gem 'annotate'
+
 group :development, :test do
   gem 'rspec-rails', '~> 3.8'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  name            :string
+#  username        :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
 
 end
