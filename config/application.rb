f6271e26bceffffd6e89c9f4b3ec437f851519cf
Tom McHenry <tommchenry@gmail.com>
2019-08-11 19:28:40 -0500

    Add middleware to application config for activadmin

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module Howler
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Middleware for ActiveAdmin
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
